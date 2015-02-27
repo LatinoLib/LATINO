7ef7eac28102e3bba449b92df5e6e383e8454f17
Petra Kralj Novak <Petra.Kralj.Novak@ijs.si>
2015-02-27 14:48:16 +0100

    RemoveCharacterRepetition fixed
    Conflicts:
    	Latino.csproj

--- TextMining/SocialMediaProcessing.cs
+++ TextMining/SocialMediaProcessing.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using Latino.TextMining;
 
-namespace Latino
+namespace Latino.TextMining
 {
     public static class SocialMediaProcessing
     {
