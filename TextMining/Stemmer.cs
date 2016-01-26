cdc1d5033e863d0f636eb7cbc739fdfb3534b3f4
mgrcar <miha.grcar@ijs.si>
2016-01-26 13:31:49 +0100

    Minor fix

--- TextMining/Stemmer.cs
+++ TextMining/Stemmer.cs
@@ -57,7 +57,7 @@ namespace Latino.TextMining
                 case Language.Dutch:
                     return new DutchStemmer();
                 case Language.Finnish:
-                    return FinnishStemmer.Instance; // exception
+                    return FinnishStemmer.Instance; // special case
                 case Language.Norwegian:
                     return new NorwegianStemmer();
                 case Language.Russian:
